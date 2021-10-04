109ac4e14b8473d2d5a3e6ec06d8f53901e8d3b5
Osmanys <ophernandez95@gmail.com>
2021-10-04 18:18:32 -0500

    regular save

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,17 +14,19 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
+
 
 export default function Squares() {
   // Use the state hook twice, as we need two slices of state: 'squares' and
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
-
+  const [currentId, setCurrentId] = useState(null)
+  const [square, setSquare] = useState(...listOfSquareIds)
   const getClassName = id => {
     // This is NOT a click handler but a helper, used inside the JSX (see below).
     // It should return a string containing the class name of 'active', if the id passed
@@ -52,7 +54,7 @@ export default function Squares() {
             <div
               id={id}
               key={id}
-              className={`square${getClassName(id)}`}
+              className={`square ${getClassName(id)}`}
               onClick={() => markActive(id)}
             >
             </div>
